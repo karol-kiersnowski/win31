64caa66a2fa042c17aa9c7fa9b87767312784e91
Karol <kargol92@gmail.com>
2020-09-23 15:34:39 +0200

    improve windows behavior

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 var desktop = document.getElementById("desktop");
-var windowProgramManager = document.getElementById("window-program-manager");
-var windowMsdos = document.getElementById("window-msdos");
+var programManager = {
+    window: document.getElementById("window-program-manager"),
+    isMouseDown: false,
+    mouseXInTitleBar: null,
+    mouseYInTitleBar: null
+};
+var msdos = {
+    window: document.getElementById("window-msdos"),
+    isMouseDown: false,
+    mouseXInTitleBar:null,
+    mouseYInTitleBar:null
+};
 
-var programManager = {isMouseDown:false, mouseXInTitleBar:null, mouseYInTitleBar:null};
-var msdos = {isMouseDown:false, mouseXInTitleBar:null, mouseYInTitleBar:null};
+function clickOnWindow(thisWindow) {
+    if (thisWindow.getAttribute("id") == "window-program-manager") {
+        programManager.window.style.zIndex = "2";
+        msdos.window.style.zIndex = "1";
+    }
+    else if (thisWindow.getAttribute("id") == "window-msdos") {
+        programManager.window.style.zIndex = "1";
+        msdos.window.style.zIndex = "2";
+    }
+}
 
 function setMouseDown(titleBar, mouse) {
     if (titleBar.parentElement.parentElement.getAttribute("id") == "window-program-manager") {
         programManager.isMouseDown = true;
-        programManager.mouseXInTitleBar = mouse.clientX - windowProgramManager.offsetLeft;
-        programManager.mouseYInTitleBar = mouse.clientY - windowProgramManager.offsetTop;
+        programManager.mouseXInTitleBar = mouse.clientX - programManager.window.offsetLeft;
+        programManager.mouseYInTitleBar = mouse.clientY - programManager.window.offsetTop;
     }
     else if (titleBar.parentElement.parentElement.getAttribute("id") == "window-msdos") {
         msdos.isMouseDown = true;
-        msdos.mouseXInTitleBar = mouse.clientX - windowMsdos.offsetLeft;
-        msdos.mouseYInTitleBar = mouse.clientY - windowMsdos.offsetTop;
+        msdos.mouseXInTitleBar = mouse.clientX - msdos.window.offsetLeft;
+        msdos.mouseYInTitleBar = mouse.clientY - msdos.window.offsetTop;
     }
 }
 
@@ -29,16 +47,16 @@ function setMouseUp(titleBar) {
 
 function tryMoveElement(mouse) {
     if (programManager.isMouseDown) {
-        windowProgramManager.style.left = mouse.clientX - programManager.mouseXInTitleBar + "px";
-        windowProgramManager.style.top = mouse.clientY - programManager.mouseYInTitleBar + "px";
+        programManager.window.style.left = mouse.clientX - programManager.mouseXInTitleBar + "px";
+        programManager.window.style.top = mouse.clientY - programManager.mouseYInTitleBar + "px";
     }
     else if (msdos.isMouseDown) {
-        windowMsdos.style.left = mouse.clientX - msdos.mouseXInTitleBar + "px";
-        windowMsdos.style.top = mouse.clientY - msdos.mouseYInTitleBar + "px";
+        msdos.window.style.left = mouse.clientX - msdos.mouseXInTitleBar + "px";
+        msdos.window.style.top = mouse.clientY - msdos.mouseYInTitleBar + "px";
     }
 }
 
 function runMSDOS() {
     //desktop.appendChild
-    windowMsdos.style.display = "block";
+    msdos.window.style.display = "block";
 }
